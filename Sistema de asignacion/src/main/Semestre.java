52bc809bf663a591f3bccfdd074522e493177118
Unknown <jcabrera16@alumnos.utalca.cl>
2018-12-10 00:34:56 -0400

    Definicion de clases

--- Sistema de asignacion/src/main/Semestre.java
+++ Sistema de asignacion/src/main/Semestre.java
@@ -9,13 +9,6 @@ package main;
  *
  * @author Javiera
  */
-public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Semestre {
     
 }
