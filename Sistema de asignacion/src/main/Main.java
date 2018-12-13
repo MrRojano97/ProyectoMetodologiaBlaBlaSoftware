40b10c635452f0f00894d1bff387fd862874bade
Unknown <lnicolas15@alumnos.utalca.cl>
2018-12-13 17:35:02 -0300

    inicializacion ventana
    ventana se inicia

--- Sistema de asignacion/src/main/Main.java
+++ Sistema de asignacion/src/main/Main.java
@@ -28,6 +28,32 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Mockup.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Mockup.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Mockup.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Mockup.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Mockup().setVisible(true);
+            }
+        });
+        
+        
     }
      public static void addCurso(String nombre){
         curso=new Curso();
