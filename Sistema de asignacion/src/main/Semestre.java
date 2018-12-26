392ab1358217ddb239144da0be2fc36f7bf322f2
matias erenchun <matiaserenchun@gmail.com>
2018-12-26 20:05:27 -0300

    se implementa semestre

--- Sistema de asignacion/src/main/Semestre.java
+++ Sistema de asignacion/src/main/Semestre.java
@@ -48,37 +48,234 @@ public class Semestre
         return this.fechaFin;
     }
     
-    
-    
-    // retorna false si el curso no se agrego
-    public boolean agregarCurso(Curso curso)
+    public void mostrarSalas()
     {
-        if(!this.cursoExiste(curso))// si el curso no existe
+        for(Sala sala: this.salasUtilizadasEnSemestre)
         {
-            return this.cursosImpartidos.add(curso);
+            // mostrar info sala.
         }
-         return false;
     }
     
-    public boolean cursoExiste(Curso curso)
+    public int agregarSala(Sala sala)
     {
+        if(0>this.salaExiste(sala.getNumero()))//si la sala no existe  se obtendra un -1
+        {
+            boolean respuesta=this.salasUtilizadasEnSemestre.add(sala);
+            if(respuesta)
+            {
+                return 1;//se  agrego correctamente  la sala.
+            }
+            else
+            {
+                return 0;//error al agregar la sala, error del metoo add.
+            }
+            
+        }
+        
+        else
+        {
+            return -2;// la sala  ya existe.
+        }
+    }
+    
+    public int salaExiste(String numeroSala)
+    {
+        int i=0;
+        for(Sala sala:this.salasUtilizadasEnSemestre)
+        {
+            if(sala.getNumero().equalsIgnoreCase(numeroSala))
+            {
+                return i;// retirna el indice de la sala si este existe
+            }
+        }
+        return -1;// retorna -1 si la sala no existe
+    }
+    
+    public Sala eliminarSala(String numeroSala)
+    {
+        Sala sala = new Sala("-1");
+        int i = this.salaExiste(numeroSala);
+        if(i>-1)// si la sala existe
+        {
+            return this.salasUtilizadasEnSemestre.remove(i);
+        }
+        else
+        {
+            return sala;// la sala  a eliminar no existe
+        }
+    }
+    
+     public void mostrarProfesores()
+    {
+        for(Profesor profesor: this.profesoresContratadosEnSemestre)
+        {
+            profesor.visualizarDatos();
+        }
+    }
+    
+    public int agregarProfesor(Profesor profesor)
+    {
+        if(0>this.profesorExiste(profesor.getNombre()))//si el profesor no existe  se obtendra un -1
+        {
+            boolean respuesta=this.profesoresContratadosEnSemestre.add(profesor);
+            if(respuesta)
+            {
+                return 1;//se  agrego correctamente  el profesor.
+            }
+            else
+            {
+                return 0;//error al agregar el Profesor, erro del metoo add.
+            }
+            
+        }
+        
+        else
+        {
+            return -2;// el rpofesor  ya existe.
+        }
+    }
+    
+    public int profesorExiste(String nombreProfesor)
+    {
+        int i=0;
+        for(Profesor profesor:this.profesoresContratadosEnSemestre)
+        {
+            if(profesor.getNombre().equalsIgnoreCase(nombreProfesor))
+            {
+                return i;// retirna el indice del profesor si este existe
+            }
+        }
+        return -1;// retorna -1 si el profesor no existe
+    }
+    
+    public Profesor eliminarProfesor(String nombreProfesor)
+    {
+        Profesor profesor = new Profesor("-1","-1","-1","-1");
+        int i =this.profesorExiste(nombreProfesor);
+        if(i>-1)// si el profesor existe
+        {
+            return this.profesoresContratadosEnSemestre.remove(i);
+        }
+        else
+        {
+            return profesor;// el profesor  a eliminar no existe
+        }
+    }
+    
+    public void mostrarCarreras()
+    {
+        for(Carrera carrera: this.carrerasImpartidasEnSemestre)
+        {
+            //mostrar carrera
+        }
+    }
+    
+    public int agregarCarrera(Carrera carrera)
+    {
+        if(0>this.CarreraExiste(carrera.getNombre()))//si la carrera ya no existe  se obtendra un -1
+        {
+            boolean respuesta=this.carrerasImpartidasEnSemestre.add(carrera);
+            if(respuesta)
+            {
+                return 1;//se  agrego correctamente  la carrera.
+            }
+            else
+            {
+                return 0;//error al agregar el curso, erro del metoo add.
+            }
+            
+        }
+        
+        else
+        {
+            return -2;// la carrera ya existe.
+        }
+    }
+    
+    public int CarreraExiste(String nombreCarrera)
+    {
+        int i=0;
+        for(Carrera carrera:this.carrerasImpartidasEnSemestre)
+        {
+            if(carrera.getNombre().equalsIgnoreCase(nombreCarrera))
+            {
+                return i;// si la carrera existe
+            }
+        }
+        return -1;// si la carrera no existe
+    }
+    
+    public Carrera eliminarCarrera(String nombreCarrera)
+    {
+        Carrera carrera = new Carrera("-1");
+        int i =this.CarreraExiste(nombreCarrera);
+        if(i>-1)// la carrera existe
+        {
+            return this.carrerasImpartidasEnSemestre.remove(i);
+        }
+        else
+        {
+            return carrera;// la carrera a eliminar no existe
+        }
+    }
+    
+    public Curso eliminarCurso(Curso curso)
+    {
+        Curso auxCurso=new Curso("-1");
+        int i=this.cursoExiste(curso);
+        if(i>-1)
+        {
+            return this.cursosImpartidos.remove(i);
+        }
+        else
+        {
+            return auxCurso;
+        }
+        
+    }
+    
+    public int agregarCurso(Curso curso)
+    {
+        if(0>this.cursoExiste(curso))// si el curso no existe
+        {
+            boolean respuesta= this.cursosImpartidos.add(curso);
+            if(respuesta)
+            {
+                return 1;//curso agregado correctamente
+            }
+            else
+            {
+                return 0;// erro al agregar curso
+            }
+        }
+        else
+        {
+            return -2;//el curso ya existe
+        }
+        
+    }
+    
+    public int cursoExiste(Curso curso)
+    {
+        int i=0;
         for(Curso auxCurso: this.cursosImpartidos)
         {
             if(auxCurso.equals(curso))
             {
-                return true;// el curso existe
+                return i;// el curso existe
             }
         }
         
-        return false;//el curso no existe
+        return -1;//el curso no existe
     }
     
+    // falta mejorar el metodo
     public void mostrarDatos()
     {
         System.out.println("semestre: "+this.nombre+", fecha de inicio: "+this.fechaInicio+", fecha de fin: "+this.fechaFin);
     }
     
-    public void mostrarCurso()
+    public void mostrarCursos()
     {
         for(Curso auxCurso: this.cursosImpartidos)
         {
